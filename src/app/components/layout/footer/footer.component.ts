498640335bedad534326f89c2bc7f6c2e120dc8c
Martin Petrov <martuto31@gmail.com>
2026-04-21 01:05:09 +0300

    Add footer

--- src/app/components/layout/footer/footer.component.ts
+++ src/app/components/layout/footer/footer.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
     selector: 'app-footer',
     templateUrl: './footer.component.html',
     styleUrl: './footer.component.css',
+    imports: [RouterLink],
 })
 
-export class FooterComponent { }
+export class FooterComponent {
+    public currentYear = new Date().getFullYear();
+}

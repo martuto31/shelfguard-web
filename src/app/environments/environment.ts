71ec386cb008ba052b8b6638320985feb986f9c4
Martin Petrov <martuto31@gmail.com>
2026-05-24 19:47:26 +0300

    Deployment update

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://shelfguard-api.up.railway.app/api/v1/',
+  apiUrl: 'https://shelfguard-api-production.up.railway.app/api/v1/',
 };
